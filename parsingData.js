f6bca725f78920a4e441aecfb24ff77b0a8c692a
MarkoVukovic <marko.vukovic95@gmail.com>
2022-06-19 14:22:10 +0200

    changedCommit

--- parsingData.js
+++ parsingData.js
@@ -1,12 +1,7 @@
-$.ajax({
-	type: "POST",
-	url: "sourceFile.csv",
-	dataType: "text",
-	data: {
-		html: csv
-	},
-	success: function (data) {
-		console.log(data);
-	}
-});
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", theUrl, true); // false for synchronous request
+	return xmlHttp.responseText;
+}
+console.log(httpGet("https://raw.githubusercontent.com/MarkoVukovic/LeverUPParseData/master/sourceFile.csv"))
 //console.log("teoi");
